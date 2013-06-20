c7edd7a0d4a244be82698e40b5d765b87e946222
Matthew Grimm <kogcyc@gmail.com>
2013-06-20 16:35:08 -0700

    init

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php
- 
-	if ($db = new SQLiteDatabase('filename')) {
-
-		$q = @$db->query('CREATE TABLE IF NOT EXISTS tablename (id int, requests int, PRIMARY KEY (id))';
-
-	}
-
-?>
+	$dbh = new PDO('pgsql:dbname=dcir2s1etumpn0;user=eqaptmujihyujc;password=qt-tywWshY0yYPNpkAaClGYD5D;host=ec2-23-21-129-81.compute-1.amazonaws.com;post=5432'); 
+?> 
