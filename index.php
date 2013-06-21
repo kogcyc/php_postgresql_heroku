0fea25d7696d9c301c2671d1db96fe34a26c5e23
Matthew Grimm <kogcyc@gmail.com>
2013-06-20 21:12:04 -0700

    init

--- index.php
+++ index.php
@@ -2,15 +2,25 @@
 
 $db = pg_connect("host='ec2-23-21-129-81.compute-1.amazonaws.com' port=5432 dbname='dcir2s1etumpn0' user='eqaptmujihyujc' password='qt-tywWshY0yYPNpkAaClGYD5D'");
 
-$result1 = pg_prepare($db, "q1", "CREATE TABLE posts (content text);");
+$query = 'CREATE TABLE posts (content text);';
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
-$result2 = pg_execute($db, "q1");
+// Printing results in HTML
+echo "<table>\n";
+while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
 
-echo $db;
+// Free resultset
+pg_free_result($result);
 
-echo "<br>";
-
-echo "done";
+// Closing connection
+pg_close($db);
 
 ?> 
 
