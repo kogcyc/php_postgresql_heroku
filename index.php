9d8f551b8dd633787488c5be30e3965bfbba9737
Matthew Grimm <kogcyc@gmail.com>
2013-06-20 21:20:39 -0700

    init

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $db = pg_connect("host='ec2-23-21-129-81.compute-1.amazonaws.com' port=5432 dbname='dcir2s1etumpn0' user='eqaptmujihyujc' password='qt-tywWshY0yYPNpkAaClGYD5D'");
 
 //$query = 'CREATE TABLE posts (content text);';
-$query = 'insert into posts (content) values ('hello');';
+$query = 'insert into posts (content) values ("hello");';
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
 $query = 'select * from posts;';
