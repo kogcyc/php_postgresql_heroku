9d7e53f39dc5d36e513f29cd46a937023e269fd6
Matthew Grimm <kogcyc@gmail.com>
2013-06-20 20:05:09 -0700

    init

--- index.php
+++ index.php
@@ -1,25 +1,11 @@
 <?php
-	$dbh = new PDO('pgsql:dbname=dcir2s1etumpn0;user=eqaptmujihyujc;password=qt-tywWshY0yYPNpkAaClGYD5D;host=ec2-23-21-129-81.compute-1.amazonaws.com;port=5432'); 
 
-//$sql = "CREATE TABLE IF NOT EXISTS post (
-//   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-//	title VARCHAR(40) NOT NULL,
-//	content VARCHAR(2000) NOT NULL
-//)";
+$db = pg_connect("host=ec2-23-21-129-81.compute-1.amazonaws.com port=5432 dbname=dcir2s1etumpn0 user=eqaptmujihyujc password=qt-tywWshY0yYPNpkAaClGYD5D");
 
-//$sq = $dbh->query($sql);
+$result = pg_prepare($db, "my_query", 'SELECT * FROM post');
 
-	//$title = htmlentities($_POST['title']);
-	//$content = htmlentities($_POST['content']);
+$result = pg_execute($db, "my_query");
 
-	$title = "Hello!";
-	$content = "Wow, that is kool!";
-
-    $sql = "INSERT INTO post (title, content) VALUES (?, ?)"; 
-    $stmt = $dbh->prepare($sql); 
-    $stmt->execute(array($title, $content)); 
-    $stmt->closeCursor();
-
-
+echo $result;
 
 ?> 
