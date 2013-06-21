746a55348ed630d5ed52e4dfeb8ed6579b9a816c
Matthew Grimm <kogcyc@gmail.com>
2013-06-20 20:28:32 -0700

    init

--- index.php
+++ index.php
@@ -2,11 +2,21 @@
 
 $db = pg_connect("host='ec2-23-21-129-81.compute-1.amazonaws.com' port=5432 dbname='dcir2s1etumpn0' user='eqaptmujihyujc' password='qt-tywWshY0yYPNpkAaClGYD5D'");
 
-$result = pg_prepare($db, "my_query", 'SELECT * FROM posts');
+$result1 = pg_prepare($db, "my_query", "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';");
 
-$result = pg_execute($db, "my_query", array(""));
+$result2 = pg_execute($db, "my_query", array(""));
 
-echo $result;
+echo $db;
+
+echo "<br>"
+
+echo $result1;
+
+echo "<br>"
+
+echo $result2;
+
+echo "<br>"
 
 echo "done";
 
