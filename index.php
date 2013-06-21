0a7d32607a25c779656d81c43ab998309f21b805
Matthew Grimm <kogcyc@gmail.com>
2013-06-20 20:36:59 -0700

    init

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 
 $db = pg_connect("host='ec2-23-21-129-81.compute-1.amazonaws.com' port=5432 dbname='dcir2s1etumpn0' user='eqaptmujihyujc' password='qt-tywWshY0yYPNpkAaClGYD5D'");
 
-$result1 = pg_prepare($db, "my_query", "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';");
+$result1 = pg_prepare($db, "q1", "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';");
 
-$result2 = pg_execute($db, "my_query");
+$result2 = pg_execute($db, "q1");
 
 echo $db;
 
