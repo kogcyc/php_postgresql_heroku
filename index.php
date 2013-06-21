c0472bddf1bcbf708b91b1ee671e18bfe8308105
Matthew Grimm <kogcyc@gmail.com>
2013-06-20 21:03:49 -0700

    init

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 $db = pg_connect("host='ec2-23-21-129-81.compute-1.amazonaws.com' port=5432 dbname='dcir2s1etumpn0' user='eqaptmujihyujc' password='qt-tywWshY0yYPNpkAaClGYD5D'");
 
-$result1 = pg_prepare($db, "q1", "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';");
+$result1 = pg_prepare($db, "q1", "CREATE TABLE posts (content text);");
 
 $result2 = pg_execute($db, "q1");
 
@@ -10,14 +10,7 @@ echo $db;
 
 echo "<br>";
 
-echo $result1;
-
-echo "<br>";
-
-echo $result2;
-
-echo "<br>";
-
 echo "done";
 
 ?> 
+
