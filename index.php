3e1496e96a54d789b417ac412688ab72c2562474
Matthew Grimm <kogcyc@gmail.com>
2013-06-20 19:25:05 -0700

    init

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 	$dbh = new PDO('pgsql:dbname=dcir2s1etumpn0;user=eqaptmujihyujc;password=qt-tywWshY0yYPNpkAaClGYD5D;host=ec2-23-21-129-81.compute-1.amazonaws.com;port=5432'); 
+
+$sql = "CREATE TABLE IF NOT EXISTS post (
+   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	title VARCHAR(40) NOT NULL,
+	content VARCHAR(2000) NOT NULL
+)";
+
+$sq = $db->query($sql);
+
+if ($sq) {
+	echo 'created';
+}
+
 ?> 
